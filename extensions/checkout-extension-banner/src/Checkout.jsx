e194da17278daf2b608a167457fcf3efe61d5082
Coralie Delpha <coralie.delpha@gmail.com>
2024-04-16 22:14:09 +0100

    Add setting collapsible

--- extensions/checkout-extension-banner/src/Checkout.jsx
+++ extensions/checkout-extension-banner/src/Checkout.jsx
@@ -12,9 +12,10 @@ export default reactExtension(
 
 function Extension() {
   const translate = useTranslate();
-  const { banner_title: merchantTitle, banner_description: merchantDescription, collapsible, banner_status: merchantStatus } = useSettings();
+  const { banner_title: merchantTitle, banner_description: merchantDescription, banner_collapsible: merchantCollapsible, banner_status: merchantStatus } = useSettings();
   const title = merchantTitle ?? translate('banner_title')
   const description = merchantDescription ?? translate('banner_description')
+  const collapsible = merchantCollapsible ?? false
   const status = merchantStatus ?? 'success'
 
   return (
