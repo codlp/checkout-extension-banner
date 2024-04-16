996c65ced1225f65afb642c381108c3c12c11c72
Coralie Delpha <coralie.delpha@gmail.com>
2024-04-16 11:47:57 +0100

    Generate extension

---
+++ extensions/checkout-extension-banner/src/Checkout.jsx
@@ -0,0 +1,22 @@
+import {
+  Banner,
+  useApi,
+  useTranslate,
+  reactExtension,
+} from '@shopify/ui-extensions-react/checkout';
+
+export default reactExtension(
+  'purchase.checkout.block.render',
+  () => <Extension />,
+);
+
+function Extension() {
+  const translate = useTranslate();
+  const { extension } = useApi();
+
+  return (
+    <Banner title="checkout-extension-banner">
+      {translate('welcome', {target: extension.target})}
+    </Banner>
+  );
+}
